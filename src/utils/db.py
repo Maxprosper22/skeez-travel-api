330aa1f9570098e66c363ac83416d6f7865ad7fb
Max Prosper <maxprosper22@gmail.com>
2025-12-02 21:54:50 -0500

    Added url parsing for db cpnnecrion string

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,6 +1,8 @@
 from asyncpg import create_pool, Pool
 from dynaconf import Dynaconf
 # from sanic.
+import urllib
+
 
 async def db_conn(config: Dynaconf) -> str:
     """ Initialize database connection """
@@ -12,12 +14,12 @@ async def db_conn(config: Dynaconf) -> str:
     DB_NAME = config["DB_NAME"]
     # DB_PASSWORD = config["DB_PASSWORD"]
     
-    dsn = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    dsn = urllib.parse.quote_plus(f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
     return dsn
 
 
 async def db_pool(dsn: str, loop) -> Pool:
     """ DB connection pool """
 
-    pool = await create_pool(dsn=dsn, loop=loop)
+    pool = await create_pool(dsn=dsn, loop=loop, statement_cache_size=0)
     return pool
