b89ff9335154a06043a01aa0445a15d70852dd17
Max Prosper <maxprosper22@gmail.com>
2025-12-03 07:04:57 -0500

    Reverted to factory pattern

--- src/main.py
+++ src/main.py
@@ -192,7 +192,5 @@ def create_app() -> Sanic:
 
     return app
 
-if __name__ == "__main__":
-    app = create_app()
-    port = int(os.getenv("PORT", 8000))
-    app.run(host="0.0.0.0", port=port, debug=False, access_log=True)
+# if __name__ == "__main__":
+    # app.run()
